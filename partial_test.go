f280e5a3ebf160f239b2867641925d7b6ed0e4ac
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-09-08 17:10:55 +0800

    Improve Partial tests

--- partial_test.go
+++ partial_test.go
@@ -2,49 +2,33 @@ package zsort_test
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
 	"testing"
 
 	"github.com/arl/zsort"
 )
 
-// func TestPartial(t *testing.T) {
-// 	s := []int{5, 7, 4, 2, 8, 6, 1, 9, 0, 3}
-// 	fmt.Println(s)
-
-// 	zsort.PartialFunc(s, 3, cmp.Less)
-// 	fmt.Println(s)
-// 	zsort.PartialFunc(s, 4, cmp.Less)
-// 	fmt.Println(s)
-// 	zsort.PartialFunc(s, len(s), cmp.Less)
-// 	fmt.Println(s)
-// }
-
 func TestPartial(t *testing.T) {
-	s := []int{5, 7, 4, 2, 8, 6, 1, 9, 0, 3}
-	fmt.Println(s)
+	sizes := []int{0, 10, 100, 1000, 10000}
+	for _, size := range sizes {
+		t.Run(fmt.Sprintf("size=%d", size), func(t *testing.T) {
+			n := size / 10
+			s := make([]int, 0, size)
+			for i := 0; i < size; i++ {
+				s = append(s, rand.Intn(10000))
+			}
 
-	zsort.Partial(s, 3)
-	fmt.Println("zsort.Partial(s, 3)")
-	fmt.Println(s)
+			zsort.Partial(s, n)
+			if !sort.IntsAreSorted(s[:n]) {
+				t.Errorf("s[:%d] should be sorted", n)
+			}
 
-	zsort.Partial(s, 4)
-	fmt.Println("zsort.Partial(s, 4)")
-	fmt.Println(s)
-
-	zsort.Partial(s, 8)
-	fmt.Println("zsort.Partial(s, 8)")
-	fmt.Println(s)
-
-	zsort.Partial(s, len(s))
-	fmt.Println("zsort.Partial(s, len(s))")
-	fmt.Println(s)
+			if n > 0 {
+				if sort.IntsAreSorted(s[n:]) {
+					t.Errorf("s[:%d] should not be sorted", n)
+				}
+			}
+		})
+	}
 }
-
-// std::array<int, 10> s {5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
-// print(s, 0);
-// std::partial_sort(s.begin(), s.begin() + 3, s.end());
-// print(s, 3);
-// std::partial_sort(s.rbegin(), s.rbegin() + 4, s.rend());
-// print(s, -4);
-// std::partial_sort(s.rbegin(), s.rbegin() + 5, s.rend(), std::greater{});
-// print(s, -5);
